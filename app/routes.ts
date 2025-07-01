6daf2a13d9521bbd765d6b66dcea89defcf721cd
Mahalakshmi Sivakumar <smahalakshmikumar@gmail.com>
2025-07-01 13:56:50 +0200

    Revert "feat:clean up redundant, make translate route as index"
    This reverts commit d30a0a6b95507badd8dab42022ee5b1992a4c903.

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/translate.tsx"),
+  index("routes/home.tsx"),
   route("translate", "routes/translate.tsx"),
 ] satisfies RouteConfig;
