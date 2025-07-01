d30a0a6b95507badd8dab42022ee5b1992a4c903
Mahalakshmi Sivakumar <smahalakshmikumar@gmail.com>
2025-07-01 13:29:49 +0200

    feat:clean up redundant, make translate route as index

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
+  index("routes/translate.tsx"),
   route("translate", "routes/translate.tsx"),
 ] satisfies RouteConfig;
